42ca536af6e407a8f4a2718683b4bf9c956ef9ad
Amrit Pal Singh <amritpalsingh01322@gmail.com>
2026-01-21 13:33:16 +0530

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'localhost',
+      },
+    ],
+  },
+  reactStrictMode: true,
+}
+
+export default nextConfig
